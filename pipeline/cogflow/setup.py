a7d960fa6d634c2488314f7f82e01193e9cf8362
Denis Gontcharov <denis@gontcharov.dev>
2020-11-13 17:52:23 +0100

    added install cogflow as GitHub action

--- pipeline/cogflow/setup.py
+++ pipeline/cogflow/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import setuptools
 
-requirements = ['apache-airflow', 'cognite-sdk']
+requirements = ["apache-airflow['postgres']", 'cognite-sdk']
 
 setuptools.setup(
     name='cogflow',
