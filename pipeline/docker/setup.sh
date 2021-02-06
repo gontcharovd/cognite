9b6c1c101a1ad509079107caf8770e8e27e83ec4
Denis Gontcharov <denis@gontcharov.dev>
2021-02-06 22:03:00 +0100

    Moving to Airflow 2.0.0 and Airflow docker-compose

---
+++ pipeline/docker/setup.sh
@@ -0,0 +1,13 @@
+echo "checking if Airflow metastore and Webserver user exist"
+(airflow users list && echo "Airflow metastore and Webserver user found. \
+				Skipping initialization") || (
+echo "Initializing Airflow metastore and creating Airflow Webserver user"
+airflow db init &&
+  airflow users create \
+    --role Admin \
+    --username "${_AIRFLOW_WWW_USER_USERNAME}" \
+    --password "${_AIRFLOW_WWW_USER_PASSWORD}" \
+    --email "${_AIRFLOW_WWW_USER_EMAIL}" \
+    --firstname "${_AIRFLOW_WWW_USER_FIRSTNAME}" \
+    --lastname "${_AIRFLOW_WWW_USER_LASTNAME}"
+)
